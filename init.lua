082562819561cb5bea56ad8bc7258777dbbf0ea1
minefaco <63024226+minefaco@users.noreply.github.com>
2024-06-14 09:44:04 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,49 @@
+--Variable global
 ranker = {}
+--Directorio del mod
+local modpath = minetest.get_modpath("ranker")
+local lfs = require("lfs")
+--Enviar debug por consola
+local log = function(msg)
+	if minetest.setting_get("log_mods") then
+		minetest.debug("[Ranker]"..msg)
+	end
+end
+
+local function getFilesInDirectory(dir)
+    local files = {}
+    for file in lfs.dir(dir) do
+        if file ~= "." and file ~= ".." then
+            local filePath = dir .. "/" .. file
+            local attr = lfs.attributes(filePath)
+            if attr.mode == "file" then
+                table.insert(files, filePath)
+            elseif attr.mode == "directory" then
+                local subFiles = getFilesInDirectory(filePath)
+                for _, subFile in ipairs(subFiles) do
+                    table.insert(files, subFile)
+                end
+            end
+        end
+    end
+    return files
+end
+
+log("Cargando contenido...")
+--Cargar mod
+local script_dirs = {
+    modpath,
+    modpath .. "/metodos",
+    modpath .. "/mods"
+}
+
+for _, dir in ipairs(script_dirs) do
+    local files = getFilesInDirectory(dir)
+    for _, file in ipairs(files) do
+        dofile(file)
+    end
+end
+
+
+
+
